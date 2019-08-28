50ea6105ba50de8b196fd1a9fb1896089518aee7
BenjaminCrozier <BenjaminCrozier+github@gmail.com>
2019-08-28 16:52:19 -0500

    demo1

---
+++ AntiPlatformer/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input.GetAxisRaw("Horizontal");   
+        Debug.Log(Input.GetAxisRaw("Horizontal"));
+
+
+    }
+
+    float horizontalMove = 0f;
+
+    void FixedUpdate(){
+
+        controller.Move(horizontalMove, false, false);
+    }
+}
