0299018870239580f51dbda643868e33304874a5
thedogsayspanic <nolenty@gmail.com>
2019-09-15 00:09:59 -0500

    Changed -EnemyAI split between ground and air Added -EnemyBase to contain non-ai specific commands

--- AntiPlatformer/Assets/Scripts/EnemyBase.cs
+++ AntiPlatformer/Assets/Scripts/EnemyBase.cs
@@ -4,15 +4,97 @@ using UnityEngine;
 
 public class EnemyBase : MonoBehaviour
 {
+    private SpriteRenderer sr;
+    private Rigidbody2D rb;
+    public Transform enemyGfx;
+    private bool invincibility = false;
+    public float jumpPower = 7.0f;
+    private bool grounded = false;
+    private Vector3 launchPad;
+    private bool m_FacingRight = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // Flip Enemy Based on Target Direction
+        if ((rb.velocity.x > 0 && !m_FacingRight) || (rb.velocity.x < 0 && m_FacingRight))
+            Flip();
+    }
+
+    public void Jump()
+    {
+        Debug.Log("Enemy Jumped");
+        rb.AddForce(new Vector3(0, jumpPower, 0), ForceMode2D.Impulse);
+        grounded = false;
+    }
+
+    public float GetBounceVelocity(Rigidbody2D rb, float minimum, float multiplier)
+    {
+        if (Mathf.Abs(rb.velocity.y) < 9f) // If velocity is too small set a minimum bounce
+            return Mathf.Abs(rb.velocity.y) + minimum;
+        else // Otherwise bounce 1.8 times your velocity
+            return Mathf.Abs(rb.velocity.y) * multiplier;
+    }
+
+    private IEnumerator Flash()
+    {
+        sr.color = new Color(sr.color.r, sr.color.b, sr.color.g, 0f);
+        yield return new WaitForSeconds(0.2f);
+        sr.color = new Color(sr.color.r, sr.color.b, sr.color.g, 1f);
+    }
+
+    public void DamageFlash()
+    {
+        StartCoroutine(Flash());
+    }
+
+    public bool GetInvincibilityState()
+    {
+        return invincibility;
+    }
+
+    public void SetInvincibilityState(bool state)
+    {
+        invincibility = state;
+        StartCoroutine(InvincibilityTimeout());
+    }
+
+    private IEnumerator InvincibilityTimeout()
+    {
+        yield return new WaitForSeconds(0.5f);
+        SetInvincibilityState(false);
+    }
+
+    public bool GetGroundedState()
+    {
+        return grounded;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        launchPad = collision.GetContact(0).normal;
+        grounded = true;
+    }
+
+    void OnCollisionExit2D()
+    {
+        launchPad = new Vector3(0, 0, 0);
+        grounded = false;
+    }
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        transform.Rotate(0f, 180f, 0f);
     }
 }
---
+++ AntiPlatformer/Assets/Scripts/GroundEnemyAI.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class GroundEnemyAI : MonoBehaviour
+{
+    public float speed = 200f;
+    public float nextWaypointDistance = 3f;
+    private Vector2 force;
+    private EnemyBase eb;
+    public Transform target;
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndOfPath = false;
+    Seeker seeker;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+        eb = GetComponent<EnemyBase>();
+
+        InvokeRepeating("UpdatePath", 0f, .5f);
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+    }
+
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (path == null)
+            return;
+
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+            reachedEndOfPath = false;
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 xDirection = new Vector2(direction.x, 0f);
+        force = xDirection * speed * Time.deltaTime;
+        if (path.vectorPath[currentWaypoint].y > rb.position.y + 2.5 && eb.GetGroundedState())
+        {
+            eb.Jump();
+        }
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < nextWaypointDistance)
+            currentWaypoint++;
+    }
+}
